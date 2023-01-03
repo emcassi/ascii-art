c6b6490b9a6836b9bdbe2567c9ae72826b5f4005
emcassi <alex.wayne.dev@gmail.com>
2023-01-03 01:17:41 -0700

    It works. I'll add color functionality next

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {InboxOutlined} from '@ant-design/icons'
+import type {UploadProps} from "antd";
+import {Upload, message} from "antd";
+
+const {Dragger} = Upload
 
 function App() {
+
+  const [ascii, setAscii] = useState("");
+
+  const props: UploadProps = {
+    name: 'file',
+    multiple: false,
+
+    action: 'http://localhost:8000/upload',
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+        setAscii(info.file.response.ascii);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log('Dropped files', e.dataTransfer.files);
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">
+          Upload a single image file (jpg, jpeg, png, webp)
+        </p>
+      </Dragger>
+      <pre className={"ascii"}>
+        {ascii}
+      </pre>
     </div>
   );
 }
